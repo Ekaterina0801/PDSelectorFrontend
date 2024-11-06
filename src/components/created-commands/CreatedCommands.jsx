8f50e9d7ddf967a8b376768351e89accd08df6ae
OlegTerV <www.oleg2002ter@mail.ru>
2024-11-06 17:22:13 +0300

    Added new content to profile

--- src/components/created-commands/CreatedCommands.jsx
+++ src/components/created-commands/CreatedCommands.jsx
@@ -1,62 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./style.css";
 
-let tags= ['JavaScript', 'React', 'Node.js'];
+let command_example = {
+  id: 1,
+  name: "Название какое то ",
+  resume: "Делаем красивое веб приложение",
+  type: "Веб-приложение",
+  tags: ["JavaScript", "React", "Node.js"],
+};
+
+let tags = ["JavaScript", "React", "Node.js"];
 
 const CreatedCommands = () => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [infoText, setInfoText] = useState("Опытный разработчик с большим опытом в разработке веб-приложений и API.");
-    const [tagsText, setTagsText] = useState(tags);
-    
-    const handleChange = (event) => {
-        setInfoText(event.target.value);
-    };
+  const [isEditing, setIsEditing] = useState(false);
+  const [name_of_command, setName] = useState(command_example.name);
+  const [resume_of_command, setResume] = useState(command_example.resume);
+  const [type_of_command, setType] = useState(command_example.type);
+  const [tags_of_command, setTags] = useState(command_example.tags);
 
-    const tagsChange = (event) => {
-        tags =event.target.value.split(",");
-        
-        setTagsText(tags);
-    };
+  const nameChange = (event) => {
+    command_example.name=event.target.value;
+    setName(command_example.name);
+  };
 
-    const toggleEdit = () => {
-        setIsEditing(prev => !prev);
-    };
+  const resumeChange = (event) => {
+    command_example.resume=event.target.value;
+    setResume(command_example.resume);
+  };
 
-    return (
-        <div className="my-resume">
-            <div className="my-resume-name">Иван Иванов</div>
-            {isEditing ? (
-                <>
-                <h5>Информация о себе:</h5>
-                <textarea className="info"
-                    type="text"
-                    value={infoText}
-                    onChange={handleChange}
-                />
-                <h5>Теги:</h5>
-                <input className="tags-input"
-                    type="text"
-                    value={tagsText}
-                    onChange={tagsChange}
-                />
-                </>
-            ) : (
-                <>
-                <div className="main-info">{infoText}</div>
-                <div className="card-tags">
+  const typeChange = (event) => {
+    command_example.type=event.target.value;
+    setType(command_example.type);
+  };
 
-                {tags.map((tag, index) => (
-                    <span key={index} className="card-tag">{tag}</span>
-                ))}
+  const tagsChange = (event) => {
+    command_example.tags = event.target.value.split(",");
+    setTags(command_example.tags);
+  };
 
-                </div>
-                </>
-            )}
-            <button className="button-input" onClick={toggleEdit}>
-                {isEditing ? 'Сохранить' : 'Редактировать'}
-            </button>
-        </div>
-    );
+  const toggleEdit = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  return (
+    <div className="my-resume">
+      {isEditing ? (
+        <>
+          <h5>Название команды:</h5>
+          <input
+            className="tags-input"
+            type="text"
+            value={name_of_command}
+            onChange={nameChange}
+          />
+          <h5>Тип проекта:</h5>
+          <input
+            className="tags-input"
+            type="text"
+            value={type_of_command}
+            onChange={resumeChange}
+          />
+          <h5>Описание проекта:</h5>
+          <textarea
+            className="info"
+            type="text"
+            value={resume_of_command}
+            onChange={typeChange}
+          />
+          <h5>Теги:</h5>
+            <input className="tags-input"
+            type="text"
+            value={tags_of_command}
+            onChange={tagsChange}
+            />
+        </>
+      ) : (
+        <>
+            <div className="card-name">{command_example.name}</div>
+            <div className="card-type">{command_example.type}</div>
+            <div className="card-resume">{command_example.resume}</div>
+            <div className="card-tags">
+              {command_example.tags.map((tag, index) => (
+                <span key={index} className="card-tag">
+                  {tag}
+                </span>
+              ))}
+            </div>
+        </>
+      )}
+      <button className="button-input" onClick={toggleEdit}>
+        {isEditing ? "Сохранить" : "Редактировать"}
+      </button>
+    </div>
+  );
 };
 
 export default CreatedCommands;
