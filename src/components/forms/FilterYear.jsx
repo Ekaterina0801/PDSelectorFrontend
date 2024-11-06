d7ec61a58f4c0bbdc0dba09b4d79adb4505f11df
OlegTerV <www.oleg2002ter@mail.ru>
2024-11-06 18:52:58 +0300

    Filter fix

--- src/components/forms/FilterYear.jsx
+++ src/components/forms/FilterYear.jsx
@@ -2,14 +2,10 @@ import './style.css';
 const FilterYear = () => {
   return (
     <div className="filter-years">
-        <span>Выберите год:</span>
+        <span>Выберите трек:</span>
     <select name="user_profile_color_1">
-        <option value="1">2020</option>
-        <option value="2">2021</option>
-        <option value="3">2022</option>
-        <option value="4">2023</option>
-        <option value="5">2024</option>
-        <option value="6">2025</option>
+        <option value="1">Бакалавры 2023-2024</option>
+        <option value="2">Магистры 2023-2024</option>
     </select>
     </div>
   );
